200c588cecffd88b6a946dce8d87e39039bf6f68
Francois Paris <fparis@3E1.42angouleme.fr>
2024-04-03 21:04:20 +0200

    struct + header

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: fparis <fparis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 19:02:32 by fparis            #+#    #+#             */
-/*   Updated: 2024/04/03 19:44:34 by fparis           ###   ########.fr       */
+/*   Updated: 2024/04/03 21:03:53 by fparis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,19 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+typedef enum	s_type
+{
+	syntax_error = -1;
+	separator;
+	command;
+	parameter;
+}	t_type;
+
 typedef struct s_arg
 {
-
+	char			*name;
+	t_type			type;
+	char			quote;
+	void			*path
+	struct s_arg	next;
 }	t_arg;
