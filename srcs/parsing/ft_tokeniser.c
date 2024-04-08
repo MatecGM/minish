b68630677ad0d564b1ef3905df407d249433d2fb
Mateo Bico <mbico@3E1.42angouleme.fr>
2024-04-08 16:57:40 +0200

    tokeniser

---
+++ srcs/parsing/ft_tokeniser.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tokeniser.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbico <mbico@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 16:44:59 by mbico             #+#    #+#             */
+/*   Updated: 2024/04/08 16:55:58 by mbico            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	countel(char *str)
+{
+	int		nb;
+	char	*ptr;
+
+	nb = 0;
+	ptr = str;
+	while(*ptr)
+	{
+		ft_strstr(ptr, "")
+		nb ++;
+	}
+}
+
+char	**ft_tokeniser(char *str)
+{
+	
+	free(str);
+}
