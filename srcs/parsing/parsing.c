20d738e4f8b039572156787b694db1dd83449bf9
Mateo Bico <mbico@3D3.42angouleme.fr>
2024-04-17 19:15:18 +0200

    parsing en cours encore

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mbico <mbico@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 20:54:13 by mbico             #+#    #+#             */
-/*   Updated: 2024/04/15 20:05:02 by mbico            ###   ########.fr       */
+/*   Updated: 2024/04/17 19:07:16 by mbico            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,21 +28,31 @@ t_type	typage(const char *elem)
 		return (tcommand);
 }
 
+void	inpipe(char **ptr)
+{
+	while(*ptr && typage(*ptr) != tpipe)
+	{
+		if (**ptr == '<' || **ptr == '>')
+			blablaredirection();
+		
+		ptr ++;
+	}
+}
+
 char	**parsing(char *str)
 {
 	char	**elem;
 	char	**ptr;
-	t_type	prevtype;
 
 	elem = ft_tokenizer(str);
 	if (!elem)
 		return (NULL);
 	ptr = elem;
-	prevtype = tfirst;
 	while (*ptr)
-	{	
-		prevtype = typage(elem);
-		ptr ++;
+	{
+		inpipe(ptr);
+		if (*ptr)
+			ptr++;
 	}
 	free(str);
 }
